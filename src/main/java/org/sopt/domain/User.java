1029c4e6b618831c1d7a18c46eba1d3473182f59
chaaehyun <connie6301@naver.com>
2025-04-30 17:32:04 +0900

    [FEAT] 회원 가입 API 구현

---
+++ src/main/java/org/sopt/domain/User.java
@@ -0,0 +1,33 @@
+package org.sopt.domain;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> postEntities = new ArrayList<>();
+
+    public User() {
+
+    }
+
+    public User(String name){
+        this.name = name;
+    }
+
+    public Long getId(){return id;}
+
+    public String getName(){return name;}
+
+    public List<Post> getPostEntities() {
+        return postEntities;
+    }
+}
--- src/main/java/org/sopt/global/response/enums/ErrorCode.java
+++ src/main/java/org/sopt/global/response/enums/ErrorCode.java
@@ -8,7 +8,10 @@ public enum ErrorCode {
     TITLE_TOO_LONG(HttpStatus.BAD_REQUEST, 40005, "제목은 30자를 초과할 수 없습니다."),
     POST_NOT_FOUND(HttpStatus.NOT_FOUND, 40401, "해당 게시글이 존재하지 않습니다."),
     TOO_EARLY_POST(HttpStatus.BAD_REQUEST, 40003, "마지막 게시글 작성 이후 3분이 지나야 합니다."),
-    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 50000, "서버 내부 오류입니다.");
+    INTERNAL_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, 50000, "서버 내부 오류입니다."),
+
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, 40402, "해당 사용자가 존재하지 않습니다."),
+    EMPTY_NAME(HttpStatus.BAD_REQUEST, 40006, "이름은 비어 있을 수 없습니다."),;
 
     private final HttpStatus httpStatus;
     private final int code;
